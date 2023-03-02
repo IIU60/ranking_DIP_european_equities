dfd4481597ae6adf0a1ffc8ce9aa76c5ec4c41f6
Hugo Perez de Albeniz <hugoperezdealbeniz@gmail.com>
2023-03-02 16:54:15 +0100

    multi-factor ranking now in app

--- code_in_production/app.py
+++ code_in_production/app.py
@@ -1,6 +1,5 @@
 import streamlit as st
 import app_functions as af
-import callbacks as cb
 import pandas as pd
 
 st.title('Stoxx600 Equities Dashboard')
@@ -10,45 +9,46 @@ with st.form(key='initial_params_form'):
     min_stocks_per_date_ratio = st.number_input('Minimum stocks per date ratio:', min_value=0.0, max_value=1.0, value=0.8)
     min_total_dates_ratio = st.number_input('Minimum total dates ratio:', min_value=0.0, max_value=1.0, value=0.8)
     data_directory_filepath = st.text_input('Filepath to data directory:', value=r'C:\Users\hugo.perezdealbeniz\Desktop\Ranking DIP European Equities\ReutersEikon\data\pivoted_data')
-    prices_csv_filepath = st.text_input('Filepath to prices csv:',value=r'C:\Users\hugo.perezdealbeniz\Desktop\Ranking DIP European Equities\ReutersEikon\data\Final Data\PriceClose.csv')
+    prices_csv_filepath = st.text_input('Filepath to prices csv:', value=r'C:\Users\hugo.perezdealbeniz\Desktop\Ranking DIP European Equities\ReutersEikon\data\Final Data\PriceClose.csv')
     init_form_button = st.form_submit_button()
 
-clean_data_dict = af.filter_data(data_directory_filepath,min_stocks_per_date_ratio,min_total_dates_ratio)[0]
-
 if 'clean_data_dict' not in st.session_state:
-    st.session_state.clean_data_dict = clean_data_dict
+    st.session_state.clean_data_dict = af.filter_data(data_directory_filepath,min_stocks_per_date_ratio,min_total_dates_ratio)[0]
+if 'create_custom_indicator' not in st.session_state:
+    st.session_state.create_custom_indicator = False
 
-#custom_factor_button = st.button('Create Multi-factor indicator',key='create_factor')
-#if custom_factor_button == True:
 
-with st.form('custom_indicator_form'):
-    indicator_name = st.text_input('Indicator Name:')
-    custom_ratio_weights_df = st.experimental_data_editor(pd.DataFrame(dict(Factor = st.session_state.clean_data_dict.keys(),Weight = 0.0)),num_rows='dynamic',width=600)
-    custom_indicator_form_button = st.form_submit_button()
+if st.button('Create Multi-factor Indicator'):
+    st.session_state.create_custom_indicator = True
 
-if custom_indicator_form_button:
-    st.balloons()
-    custom_indicator_df = af.multi_factor_ranking(custom_ratio_weights_df, st.session_state.clean_data_dict, n_quantiles)
-    st.session_state.clean_data_dict[indicator_name] = custom_indicator_df
+if st.session_state.create_custom_indicator == True:
+    with st.form('custom_indicator_form'):
+        indicator_name = st.text_input('Indicator Name:')
+        custom_ratio_weights_df = st.experimental_data_editor(pd.DataFrame(dict(Factor=st.session_state.clean_data_dict.keys(), Weight=0.0)),num_rows='dynamic',width=600)
+        custom_indicator_form_button = st.form_submit_button()
+    
+    if custom_indicator_form_button:
+        st.balloons()
+        custom_indicator_df = af.multi_factor_ranking(custom_ratio_weights_df, st.session_state.clean_data_dict, n_quantiles)
+        st.session_state.clean_data_dict[indicator_name] = custom_indicator_df
+        st.session_state.create_custom_indicator = False
+        st.experimental_rerun()
+selected_ratio = st.selectbox('Ratio:', st.session_state.clean_data_dict.keys())
 
-selected_ratio = st.selectbox('Ratio:',st.session_state.clean_data_dict.keys())
-
-ranked_data = af.rank_data(st.session_state.clean_data_dict[selected_ratio],n_quantiles)
-rents_df = af.get_rents_df(ranked_data,prices_csv_filepath,n_quantiles)
-
-desired_graphs = st.multiselect('Desired Graphs:',['NAV Absoluto','NAV Relativo a Equiponderado','Rentabilidad Media Anualizada', 'Volatilidad Anualizada', 'Sharpe Ratio'])
-
-log_scale = st.checkbox('Logarithmic Scale:')
+ranked_data = af.rank_data(st.session_state.clean_data_dict[selected_ratio], n_quantiles)
+rents_df = af.get_rents_df(ranked_data, prices_csv_filepath,n_quantiles)
 
 graphs_dict = {
     'NAV Absoluto': af.plot_NAV_absoluto,
     'NAV Relativo a Equiponderado': af.plot_NAV_relativo,
     'Rentabilidad Media Anualizada': af.plot_rentabilidad_media,
     'Volatilidad Anualizada':af.plot_volatilidad,
-    'Sharpe Ratio':af.plot_sharpe}
+    'Sharpe Ratio':af.plot_sharpe
+    }
 colors = ["#0068c9","#d7dce6","#7f51b5","#ffd578","#ff902d","#8af0aa","#2db19f","#ffb1b1","#ff3030","#83c9ff",'#000000']
+
+desired_graphs = st.multiselect('Desired Graphs:',graphs_dict.keys())
+log_scale = st.checkbox('Logarithmic Scale:')
 
 for graph in desired_graphs:
     st.plotly_chart(graphs_dict[graph](rents_df,colors,log_scale))
-
-st.session_state
