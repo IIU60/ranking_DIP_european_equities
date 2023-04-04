463175fd2fcdb2cdc37af07786e3095b634b1107
Hugo Perez de Albeniz <hugoperezdealbeniz@gmail.com>
2023-04-04 14:58:16 +0200

    added masking to the workflow and made everything stateful

--- code_in_production/app.py
+++ code_in_production/app.py
@@ -4,22 +4,30 @@ import plots as pl
 import pandas as pd
 import custom_calculations as calcs
 
-
 #import matplotlib.pyplot as plt
 st.set_page_config(layout='wide')
 st.title('Stoxx600 Equities Dashboard')
 
 with st.sidebar:
+
     with st.form(key='initial_params_form'):
         n_quantiles = st.number_input('Number of Quantiles:', min_value=1, value=10)
         min_stocks_per_date_ratio = st.number_input('Minimum stocks per date ratio:', min_value=0.0, max_value=1.0, value=0.8)
         min_total_dates_ratio = st.number_input('Minimum total dates ratio:', min_value=0.0, max_value=1.0, value=0.8)
         data_directory_filepath = st.text_input('Filepath to data directory:', value=r'C:\Users\hugo.perezdealbeniz\Desktop\Ranking DIP European Equities\ReutersEikon\data\pivoted_data')
         prices_csv_filepath = st.text_input('Filepath to prices csv:', value=r'C:\Users\hugo.perezdealbeniz\Desktop\Ranking DIP European Equities\ReutersEikon\data\Final Data\PriceClose.csv')
+        mask_filepath = st.text_input('Filepath to index constituency mask:', value=r'C:\Users\hugo.perezdealbeniz\Desktop\Ranking DIP European Equities\ReutersEikon\data\PriceClose_vertical\monthly_constituents_filter.csv')
         init_form_button = st.form_submit_button()
 
-    if 'clean_data_dict' not in st.session_state:
-        st.session_state.clean_data_dict = af.filter_data(data_directory_filepath,min_stocks_per_date_ratio,min_total_dates_ratio)[0]    
+    if 'start_app' not in st.session_state:
+        st.session_state.start_app = False
+        
+    if init_form_button:
+        st.session_state.clean_data_dict = af.filter_data(fr'{data_directory_filepath}',min_stocks_per_date_ratio,min_total_dates_ratio)[0]
+        st.session_state.prices_df = pd.read_csv(fr'{prices_csv_filepath}',index_col=0)
+        st.session_state.mask = pd.read_csv(fr'{mask_filepath}',index_col=0)
+        st.session_state.start_app = True
+        
  
     if 'create_weighted_indicator' not in st.session_state:
         st.session_state.create_weighted_indicator = False
@@ -62,37 +70,36 @@ with st.sidebar:
             st.session_state.create_custom_indicator = False
             st.experimental_rerun()
 
-n_indicators = st.number_input('Number of Indicators:',value=1)
+if st.session_state.start_app == True:
+    n_indicators = st.number_input('Number of Indicators:',value=1)
 
-prices_df = pd.read_csv(fr'{prices_csv_filepath}',index_col=0) # add to session state
+    cols = st.columns(n_indicators)
+    for i in range(n_indicators):
+        with cols[i]:
+            
+            selected_ratio = st.selectbox('Ratio:', st.session_state.clean_data_dict.keys(),key=f'ratio_{i}')
+            
+            type_of_indicator = st.selectbox('Type:', ['alto','bajo'],key=f'type_{i}')
+            log_scale = st.checkbox('Logarithmic Scale:',key=f'log_graphs_{i}')
+            masked_data = af.apply_mask(st.session_state.clean_data_dict[selected_ratio],st.session_state.mask)
+            ranked_data = af.rank_data(masked_data, n_quantiles, type_of_indicator)
+            #fig,ax = plt.subplots()
+            #ax.plot(ranked_data.notna().sum(axis=1))
+            #st.pyplot(fig)
+            rents_df = af.get_rents_df(ranked_data, st.session_state.prices_df, n_quantiles,1,1)
+            st.write(len(rents_df)/277)
+            graphs_dict = {
+                'NAV Absoluto': pl.plot_NAV_absoluto,
+                'NAV Relativo a Equiponderado': pl.plot_NAV_relativo,
+                'Rentabilidad Media Anualizada': pl.plot_rentabilidad_media,
+                'Volatilidad Anualizada':pl.plot_volatilidad,
+                'Sharpe Ratio':pl.plot_sharpe
+                }
 
-cols = st.columns(n_indicators)
-for i in range(n_indicators):
-    with cols[i]:
+            desired_graphs = st.multiselect('Desired Graphs:',graphs_dict.keys(),key=f'desired_graphs_{i}')
         
-        selected_ratio = st.selectbox('Ratio:', st.session_state.clean_data_dict.keys(),key=f'ratio_{i}')
-        
-        type_of_indicator = st.selectbox('Type:', ['alto','bajo'],key=f'type_{i}')
-        log_scale = st.checkbox('Logarithmic Scale:',key=f'log_graphs_{i}')
-        ranked_data = af.rank_data(st.session_state.clean_data_dict[selected_ratio], n_quantiles, type_of_indicator)
-        #fig,ax = plt.subplots()
-        #ax.plot(st.session_state.clean_data_dict[selected_ratio].notna().sum(axis=1))
-        #st.pyplot(fig)
-        #fig,ax = plt.subplots()
-        #ax.plot(ranked_data.notna().sum(axis=1))
-        #st.pyplot(fig)
-        rents_df = af.get_rents_df(ranked_data, prices_df, n_quantiles, 1,1)
-        st.write(len(rents_df)/277)
-        graphs_dict = {
-            'NAV Absoluto': pl.plot_NAV_absoluto,
-            'NAV Relativo a Equiponderado': pl.plot_NAV_relativo,
-            'Rentabilidad Media Anualizada': pl.plot_rentabilidad_media,
-            'Volatilidad Anualizada':pl.plot_volatilidad,
-            'Sharpe Ratio':pl.plot_sharpe
-            }
+            for graph in desired_graphs:
+                st.plotly_chart(graphs_dict[graph](rents_df, selected_ratio, log_scale=log_scale),True)
 
-        desired_graphs = st.multiselect('Desired Graphs:',graphs_dict.keys(),key=f'desired_graphs_{i}')
-       
-        for graph in desired_graphs:
-            st.plotly_chart(graphs_dict[graph](rents_df, selected_ratio, log_scale=log_scale),True)
-
+else:
+    st.warning('Please Submit the data and params form in the sidebar to load the app.')
