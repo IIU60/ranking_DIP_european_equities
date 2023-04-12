8ae0ac376e58d86bce02e00b18c3da05f155b7ca
Hugo Perez de Albeniz <hugoperezdealbeniz@gmail.com>
2023-04-12 09:08:20 +0200

    added function documentation to app

--- code_in_production/app.py
+++ code_in_production/app.py
@@ -4,7 +4,6 @@ import plots as pl
 import pandas as pd
 import custom_calculations as calcs
 
-#import matplotlib.pyplot as plt
 st.set_page_config(layout='wide')
 st.title('Stoxx600 Equities Dashboard')
 
@@ -14,8 +13,8 @@ with st.sidebar:
         n_quantiles = st.number_input('Number of Quantiles:', min_value=1, value=10)
         min_stocks_per_date_ratio = st.number_input('Minimum stocks per date ratio:', min_value=0.0, max_value=1.0, value=0.8)
         min_total_dates_ratio = st.number_input('Minimum total dates ratio:', min_value=0.0, max_value=1.0, value=0.8)
-        data_directory_filepath = st.text_input('Filepath to data directory:', value=r'C:\Users\hugo.perezdealbeniz\Desktop\Ranking DIP European Equities\ReutersEikon\data\pivoted_data')
-        prices_csv_filepath = st.text_input('Filepath to prices csv:', value=r'C:\Users\hugo.perezdealbeniz\Desktop\Ranking DIP European Equities\ReutersEikon\data\Final Data\PriceClose.csv')
+        data_directory_filepath = st.text_input('Filepath to data directory:', value=r'C:\Users\hugo.perezdealbeniz\Desktop\Ranking DIP European Equities\ReutersEikon\data\vertical_dowload_files')
+        prices_csv_filepath = st.text_input('Filepath to prices csv:', value=r'C:\Users\hugo.perezdealbeniz\Desktop\Ranking DIP European Equities\ReutersEikon\data\vertical_dowload_files\PriceClose.csv')
         mask_filepath = st.text_input('Filepath to index constituency mask:', value=r'C:\Users\hugo.perezdealbeniz\Desktop\Ranking DIP European Equities\ReutersEikon\data\PriceClose_vertical\monthly_constituents_filter.csv')
         init_form_button = st.form_submit_button()
 
@@ -61,15 +60,23 @@ with st.sidebar:
             user_input = st.text_input('Formula:')
             custom_indicator_form_button = st.form_submit_button()
             calcs.mask = st.session_state.mask
-            locals_dict = {k:v for k,v in calcs.__dict__.items() if not k.startswith('__')}
+            calcs_dict = {k:v for k,v in calcs.__dict__.items() if not k.startswith('__') and k not in ['ta','pd','apply_mask','mask']}
+            locals_dict = calcs_dict.copy()
             locals_dict.update(st.session_state.clean_data_dict)
-            st.write(locals_dict.keys()) # write docs to screen
+            docstrings = {k:v.__doc__ for k, v in calcs_dict.items()}
+            
+            with st.expander('Available Functions documentation'):
+                for key, value in docstrings.items():
+                    st.markdown(f"**{key}**")
+                    st.markdown(value)    
         
+            
         if custom_indicator_form_button:
-            custom_df = eval(user_input,{},locals_dict)
+            custom_df = eval(user_input,{'__builtins__':{}},locals_dict)
             st.session_state.clean_data_dict[indicator_name] = custom_df
             st.session_state.create_custom_indicator = False
             st.experimental_rerun()
+
 
 if st.session_state.start_app == True:
     n_indicators = st.number_input('Number of Indicators:',value=1)
