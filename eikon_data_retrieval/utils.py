6bb5d8cb62e6a3f40f594374f80406129cd9089d
IIU60 <hugoperezdealbeniz@gmail.com>
2023-04-26 10:33:25 +0200

    created dates list last day of month function

--- eikon_data_retrieval/utils.py
+++ eikon_data_retrieval/utils.py
@@ -13,11 +13,11 @@ def create_df_mask_from_json(json_fp,output_fp):
 
 
 def create_dates_list(type_=['days','months'], start_date=(2000,1,1), end_date=(2023,1,1), as_str=False, only_weekdays=False):
-    start_date = dt.date(start_date[0],start_date[1],start_date[2])
-    end_date = dt.date(end_date[0],end_date[1],end_date[2])
+    start_date = dt.date(*start_date)
+    end_date = dt.date(*end_date)
+    
     dates = [start_date]
     i = 0
-
     if type_=='days':
         while (date := start_date + relativedelta(days=i+1)) <= end_date:
             dates.append(date)
@@ -41,3 +41,17 @@ def create_dates_list(type_=['days','months'], start_date=(2000,1,1), end_date=(
         return list(map(str,dates))
     
     return dates
+
+def dates_list_last_day_of_month(start_date=(2000,1,1),end_date=(2023,4,26)):
+
+    start_date = dt.date(*start_date)
+    end_date = dt.date(*end_date)
+
+    def last_day_of_month(year, month):
+        next_month = dt.date(year, month, 1) + dt.timedelta(days=32)
+        return next_month.replace(day=1) - dt.timedelta(days=1)
+    
+    last_days = [date for year in range(start_date.year,end_date.year+1) for month in range(1,13) if (start_date < (date:=last_day_of_month(year,month)) and date < end_date)]
+
+    return last_days
+
