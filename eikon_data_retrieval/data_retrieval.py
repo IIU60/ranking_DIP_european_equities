798159cdeae32df0d65db6aaf73ecfb552abd203
IIU60 <hugoperezdealbeniz@gmail.com>
2023-05-26 11:04:43 +0200

    modified vertical_download to consider data errors in the eikon err dict, fixed bug in the retrying fails snippet, removed default arguments from reconstruction, moved yearly data 12 month ffill into reconstruction, moved ek.set_app_key into vertical download

--- eikon_data_retrieval/data_retrieval.py
+++ eikon_data_retrieval/data_retrieval.py
@@ -5,7 +5,6 @@ import utils
 import os
 
 from dotenv import load_dotenv; load_dotenv(r'..\.env') #load environment variables from .env file
-ek.set_app_key(os.environ['EIKON_APP_KEY']) #set Eikon API key
 
 #dictionary of known frequencies
 frequencies_dict = {
@@ -16,12 +15,14 @@ frequencies_dict = {
 }
 
 #function to download data for a specified field, using the Eikon API
-def vertical_download(field_name:str,field_function,instruments_list,parameters,saving_directory_fp):
+def vertical_download(field_name:str, field_function:str, instruments_list:list, parameters:dict ,saving_directory_fp:str,make_new_dir:bool=True):
     
+    ek.set_app_key(os.environ['EIKON_APP_KEY']) #set Eikon API key
+    #if make_new_dir==True:    
     i = 0
     while i != -1: #create unique directory for field data, even if one with the same name already exists
         try:
-            dir_to_save = os.path.join(saving_directory_fp,field_name)
+            dir_to_save = os.path.join(saving_directory_fp, field_name)
             os.mkdir(dir_to_save)
             i = -1
         except FileExistsError:
@@ -44,59 +45,64 @@ def vertical_download(field_name:str,field_function,instruments_list,parameters,
         except Exception:
             fails.append(instrument)
             continue
+        if err is not None:
+            fails.append(instrument)
+            continue
         df.to_csv(f"{raw_data_dir_fp}/{instrument}.csv") #save data to CSV file
         dfs_list.append(df)
 
     #retry any failed downloads
-    if not fails:
+    if fails:
         print('Retrying fails:')
         for instrument in tqdm(fails[:]):
             try:
                 df, err = ek.get_data(instrument, fields, parameters)
-                fails.remove(instrument)
             except Exception:
                 continue
+            if err is not None:
+                continue
+            fails.remove(instrument)
             df.to_csv(f"{raw_data_dir_fp}/{instrument}.csv")
             dfs_list.append(df)
         print('Failed twice for:\n',fails)
 
-    return dfs_list
+    return dfs_list, dir_to_save
 
 
 #function to create a list of dataframes from CSV files in a specified directory
-def dfs_list_from_dir(dir_fp):
+def dfs_list_from_dir(dir_fp:str):
     dfs_list = []
     for i in os.listdir(dir_fp):
-        dfs_list.append(pd.read_csv(os.path.join(dir_fp,i),index_col=0))
+        dfs_list.append(pd.read_csv(os.path.join(dir_fp,i), index_col=0))
     return dfs_list
 
 
 #function to reconstruct a complete dataframe from a list of dataframes with missing values,
 #using fill-forward interpolation up to 7 days of data
-def reconstruction(dfs_list:list, start_date:tuple=(2000,1,1), end_date:tuple=(2023,1,1), desired_type_of_dates:str='monthly', day_of_week:int=None):
+def reconstruction(dfs_list:list, start_date:tuple, end_date:tuple, desired_type_of_dates:str, day_of_week:int=None):
     types_of_dates = ['daily','weekly','first_of_month','monthly','yearly'] #list of known types of dates
     if desired_type_of_dates not in types_of_dates:
         raise ValueError('type_of_date: %s is not in %s'% (desired_type_of_dates,types_of_dates))
     
     concated_df = pd.concat(dfs_list) #concatenate dataframes into one
 
-    processed_df = concated_df.replace(['NaN',''],pd.NA) #replace NaN and empty strings with pandas NA
-    processed_df = processed_df.dropna().drop_duplicates(subset=['Date','Instrument']) #drop rows with NA or duplicate values
+    processed_df = concated_df.replace(['NaN',''], pd.NA) #replace NaN and empty strings with pandas NA
+    processed_df = processed_df.dropna(how='all', axis=1).dropna().drop_duplicates(subset=['Date','Instrument']) #drop rows with NA or duplicate values
     processed_df['Date'] = processed_df.Date.apply(lambda x: x[:10]) #extract date from timestamp column
 
-    pivoted_df = processed_df.pivot(index='Date',columns='Instrument',values=list(set(processed_df.columns)-set(['Date','Instrument']))[0]) #pivot table to rearrange instrument data by date
+    pivoted_df = processed_df.pivot(index='Date', columns='Instrument', values=list(set(processed_df.columns)-set(['Date','Instrument']))[0]) #pivot table to rearrange instrument data by date
 
-    days_list = utils.create_dates_list('daily',start_date=start_date,end_date=end_date,as_str=True) #create list of all days in date range
+    days_list = utils.create_dates_list('daily', start_date=start_date, end_date=end_date, as_str=True) #create list of all days in date range
     
     #determine desired dates based on desired type of dates and other parameters
     if desired_type_of_dates == 'first_of_month':
-        selection_of_dates = utils.create_dates_list('months',start_date=start_date,end_date=end_date,as_str=True)
+        selection_of_dates = utils.create_dates_list('months',start_date=start_date, end_date=end_date, as_str=True)
     elif desired_type_of_dates in ['monthly','yearly']:
-        selection_of_dates = utils.dates_list_last_day_of_month(start_date,end_date,as_str=True)
+        selection_of_dates = utils.dates_list_last_day_of_month(start_date, end_date, as_str=True)
     elif desired_type_of_dates == 'daily':
         selection_of_dates = days_list
     elif desired_type_of_dates == 'weekly':
-        selection_of_dates = utils.create_dates_list('weekly',start_date,end_date,as_str=True,day_of_week=day_of_week)
+        selection_of_dates = utils.create_dates_list('weekly', start_date, end_date, as_str=True, day_of_week=day_of_week)
     
     dates_dict = {}
     for i in days_list:
@@ -104,16 +110,20 @@ def reconstruction(dfs_list:list, start_date:tuple=(2000,1,1), end_date:tuple=(2
     dates_dict.update(pivoted_df.T.to_dict())
     complete_dates_df = pd.DataFrame(dates_dict).T.sort_index() #create a dataframe with desired dates but missing values for instruments not found in original dataframes
     
-    filled_df = complete_dates_df.fillna(method='ffill',limit=7) #fill missing values using a fill-forward method
+    filled_df = complete_dates_df.fillna(method='ffill', limit=7) #fill missing values using a fill-forward method
     
     final_df = filled_df.loc[selection_of_dates] #selecting the desired dates frequency
+
+    if desired_type_of_dates == 'yearly': #fill any remaining gaps in extra-monthly data
+        final_df = final_df.fillna(method='ffill', limit=12)
+
     final_df = final_df.apply(pd.to_numeric) #format dataframe to contain only numeric values
 
     return final_df
 
 
 #function to determine the frequency of data, based on the freq parameter and the frequencies_dict
-def find_freq(freq,frequencies_dict):
+def find_freq(freq:str, frequencies_dict:dict):
     for key, freqs_list in frequencies_dict.items():
         if freq in freqs_list:
             return key
@@ -122,10 +132,8 @@ def find_freq(freq,frequencies_dict):
 
 #main function to download and process data for specified fields and instruments, saving to CSV files in a specified directory
 #using fill-forward interpolation for up to 12 months for extra-monthly data frequencies
-def download_indicators(fields_list:list,instruments_list:list,parameters:dict,saving_directory_fp:str):
+def download_indicators(fields_list:list, instruments_list:list, parameters:dict, saving_directory_fp:str):
     
-    #os.chdir(fr'{saving_directory_fp}')
-
     start_date = tuple(map(int,parameters.get('SDate').split('-'))) #parse start date parameter
     end_date = tuple(map(int,parameters.get('EDate').split('-'))) #parse end date parameter
     freq = parameters.get('Frq') #parse frequency parameter
@@ -136,15 +144,24 @@ def download_indicators(fields_list:list,instruments_list:list,parameters:dict,s
 
         field_name = field_function.split('.')[-1]
         
-        desired_type_of_dates = find_freq(freq,frequencies_dict) #determine desired type of dates based on frequency parameter
-        dfs_list = vertical_download(field_name,field_function,instruments_list,parameters,saving_directory_fp) #download data for field and instruments
-        complete_df = reconstruction(dfs_list=dfs_list,start_date=start_date,end_date=end_date,desired_type_of_dates=desired_type_of_dates) #reconstruct complete dataframe from downloaded data
+        desired_type_of_dates = find_freq(freq, frequencies_dict) #determine desired type of dates based on frequency parameter
+        dfs_list, dir_name = vertical_download(field_name, field_function, instruments_list, parameters, saving_directory_fp) #download data for field and instruments
+        complete_df = reconstruction(dfs_list=dfs_list, start_date=start_date, end_date=end_date, desired_type_of_dates=desired_type_of_dates) #reconstruct complete dataframe from downloaded data
         
-        if freq in frequencies_dict['yearly']: #fill any remaining gaps in extra-monthly data
-            complete_df = complete_df.fillna(method='ffill',limit=12)
-        
-        complete_df.to_csv(f'{field_name}/{field_name}.csv') #save complete dataframe to CSV file
+        complete_df.to_csv(f'{dir_name}/{field_name}.csv') #save complete dataframe to CSV file
         
         data_dict[field_name] = complete_df
     
     return data_dict
+
+#def continue_download(field_function:str, dir_fp:str, full_instruments_list:list, parameters:dict):
+#    already_downloaded = set(map(lambda x:x.strip('.csv'),os.listdir(os.path.join(dir_fp,'raw_data'))))
+#    instruments_list = list(set(full_instruments_list)-already_downloaded)
+#
+#    start_date = tuple(map(int,parameters.get('SDate').split('-'))) #parse start date parameter
+#    end_date = tuple(map(int,parameters.get('EDate').split('-'))) #parse end date parameter
+#    freq = parameters.get('Frq') #parse frequency parameter
+
+
+## add makedir parameter to vertical download
+## if False just download into specified directory
